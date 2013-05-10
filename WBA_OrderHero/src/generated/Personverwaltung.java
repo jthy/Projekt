538e8053bc09f9bdcd7bee9dd5756869ee7ca0ae
juliathyssen <juliathyssen@192.168.2.1>
2013-05-10 11:19:13 +0200

    xml-Dateien generiert

---
+++ WBA_OrderHero/src/generated/Personverwaltung.java
@@ -0,0 +1,29 @@
+package generated;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.sql.Timestamp;
+import java.util.Scanner;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+
+public class Personverwaltung {
+	
+	static Scanner scanner = new Scanner(System.in);
+	private static Scanner input;
+	
+	public static void main(String[] args) throws JAXBException IOException{
+		Boolean anzeige = true;
+		int eingabe = 0;
+		
+		//xml_person in Datei speichern
+		File person = new File("src/Person.xml");
+	}
+	
+}
