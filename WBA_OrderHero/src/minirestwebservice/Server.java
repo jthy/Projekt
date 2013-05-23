bb25b8bde488f225ccc0625760a1fb02f7217e96
juliathyssen <juliathyssen@192.168.0.122>
2013-05-23 13:55:30 +0200

    fv

--- WBA_OrderHero/src/minirestwebservice/Server.java
+++ WBA_OrderHero/src/minirestwebservice/Server.java
@@ -10,12 +10,12 @@ public class Server {
 	 */
 	public static void main(String[] args) throws Exception 
 	{
-		String url = "http://localhost:4444";
+		String url = "http://localhost:4434";
 		
 		SelectorThread srv = GrizzlyServerFactory.create (url );
 		
 		System.out.println( "URL: " + url);
-		Thread.sleep( 1000 * 60 * 10);
+		Thread.sleep( 1000 * 60 * 5);
 		srv.stopEndpoint();
 	}
 	
